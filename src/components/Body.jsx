bf47ae7c392cd282a5ea79b4cf86b98407f2d185
Henrique Siqueira Lima <henrynyyls@gmail.com>
2024-12-28 20:26:11 -0300

    Added a basic setup to test if the scraping is working(it is)

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,52 +1,30 @@
 import React, { useState, useEffect } from "react";
-import puppeteer from "puppeteer";
 
 function Body() {
   const [activeSupermarket, setActiveMarket] = useState("Olho D' Água");
 
   useEffect(() => {
-    console.log(activeSupermarket);
+    const fetchData = async () => {
+      console.log("Começo do fetch");
+      try {
+        console.log("fazendo fetch");
+        const response = await fetch("http://localhost:5000/scrape");
+
+        // Verifique se a resposta é bem-sucedida
+        if (!response.ok) {
+          console.log("Houve um erro");
+          throw new Error("Erro na resposta da API");
+        }
+        console.log("Response:", response);
+        const data = await response.json();
+        console.log(data); // Verifique o que está sendo retornado
+      } catch (error) {
+        console.error("Erro ao buscar os dados:", error);
+      }
+    };
+
+    fetchData();
   }, [activeSupermarket]);
-
-  async function scrapeSupermarket(url) {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-
-    try {
-      await page.goto(url, { waitUntil: "domcontentloaded" });
-
-      // Extrair dados dos produtos
-      const products = await page.evaluate(() => {
-        const items = [];
-
-        // Selecionar todas as divs com a classe shelf-item (pai)
-        document.querySelectorAll(".shelf-item").forEach((product) => {
-          const price = product
-            .querySelector(".best-price strong")
-            ?.textContent.trim(); // Preço
-          const image = product.querySelector(".shelf-item__image img")?.src; // URL da imagem
-          const title = product
-            .querySelector(".shelf-item__title")
-            ?.textContent.trim(); // Título
-
-          if (price && image && title) {
-            items.push({ price, image, title });
-          }
-        });
-
-        return items;
-      });
-
-      console.log(products); // Mostrar os produtos capturados
-      await browser.close();
-    } catch (error) {
-      console.error("Erro ao fazer scraping:", error);
-      await browser.close();
-    }
-  }
-
-  // Substitua a URL pelo site do supermercado
-  //scrapeSupermarket("https://example-supermarket.com");
 
   return (
     <div className="bg-red-600 w-full h-full">
