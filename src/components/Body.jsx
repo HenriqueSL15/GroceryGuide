8b1d8e0918390b45c73897bf07078bd873eb007a
Henrique Siqueira Lima <henrynyyls@gmail.com>
2024-12-29 22:20:12 -0300

    The base mechanic of scraping is done
    - If you change the url for any of the Comper urls it will work, it will get the price, name and the image src.

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,28 +1,68 @@
 import React, { useState, useEffect } from "react";
 
 function Body() {
-  const [activeSupermarket, setActiveMarket] = useState("Olho D' Água");
+  const links = [
+    {
+      comper: {
+        alimentacao_saudavel: "https://www.comper.com.br/alimentacao-saudavel",
+        bebidas: "https://www.comper.com.br/bebidas",
+        carnes: "https://www.comper.com.br/carnes-aves-e-peixes",
+        congelados: "https://www.comper.com.br/congelados",
+        frios: "https://www.comper.com.br/frios-e-laticinios",
+        higiene: "https://www.comper.com.br/higiene-e-beleza",
+        hortifruti: "https://www.comper.com.br/hortifruti",
+        infantil: "https://www.comper.com.br/infantil",
+        limpeza: "https://www.comper.com.br/limpeza",
+        matinais: "https://www.comper.com.br/matinais",
+        mercearia: "https://www.comper.com.br/mercearia",
+        padaria: "https://www.comper.com.br/padaria",
+        pet_shop: "https://www.comper.com.br/pet_shop",
+        doces: "",
+        perfumaria: "",
+        outras_categorias: "",
+      },
+      olho_agua: {},
+    },
+  ];
+
+  const [activeSupermarket, setActiveMarket] = useState("comper");
 
   useEffect(() => {
-    const fetchData = async () => {
-      console.log("Começo do fetch");
+    const sendInfo = async () => {
       try {
-        console.log("fazendo fetch");
+        const information = links[0][activeSupermarket];
+        const response = await fetch("http://localhost:5000/info", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(information),
+        });
+
+        //Verifica se a resposta foi bem-sucedida
+        if (!response.ok) {
+          throw new Error("Erro no envio de informações");
+        }
+        const data = await response.json();
+      } catch (error) {
+        console.error("Erro ao enviar as informações:", error);
+      }
+    };
+    const fetchData = async () => {
+      try {
         const response = await fetch("http://localhost:5000/scrape");
 
         // Verifique se a resposta é bem-sucedida
         if (!response.ok) {
-          console.log("Houve um erro");
           throw new Error("Erro na resposta da API");
         }
-        console.log("Response:", response);
         const data = await response.json();
         console.log(data); // Verifique o que está sendo retornado
       } catch (error) {
         console.error("Erro ao buscar os dados:", error);
       }
     };
-
+    sendInfo();
     fetchData();
   }, [activeSupermarket]);
 
@@ -34,7 +74,7 @@ function Body() {
           <div className="flex flex-col text-xl justify-start items-start gap-2">
             <button
               className="font-light hover:font-normal transition-all"
-              onClick={() => setActiveMarket("olho_d_agua")}
+              onClick={() => setActiveMarket("olho_agua")}
             >
               Olho D' Água
             </button>
